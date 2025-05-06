ac75d4f93b680e5af88c5cf42736542144d50c4c
Your Name <you@example.com>
2025-05-06 17:59:13 +0100

    Modified build.sh src directory

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ export PICO_SDK_PATH=$(pwd)/pico-sdk
 export PICO_BOARD=pico2_w
 export PICO_PLATFORM=rp2350
 
-cmake -S . -B build \
+cmake -S src -B build \
     -DFREERTOS_KERNEL_PATH=$FREERTOS_KERNEL_PATH \
     -DPICO_SDK_PATH=$PICO_SDK_PATH \
     -DPICO_BOARD=$PICO_BOARD \
